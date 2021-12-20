088616501ee1edf85f877c3c3ebdac40d894ddb8
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-20 20:24:25 -0300

    adjust:rootReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,10 +2,7 @@ import { combineReducers } from 'redux';
 import loadingReducer from './ducks/Loading';
 import userReducer from './ducks/User';
 
-const rootReducer = () =>
-  combineReducers({
-    userReducer,
-    loadingReducer,
-  });
-
-export default rootReducer;
+export default combineReducers({
+  userReducer,
+  loadingReducer,
+});
