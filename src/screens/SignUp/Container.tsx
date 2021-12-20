43e6868468c7f0d714ecbc0f10a2cfb3556f4fc9
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-20 20:26:03 -0300

    feat: add logic to signup container

--- src/screens/SignUp/Container.tsx
+++ src/screens/SignUp/Container.tsx
@@ -1,27 +1,41 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { createUser } from '../../redux/ducks/User';
+import { createUser, User } from '../../redux/ducks/User';
+import { startLoading, stopLoading } from '../../redux/ducks/Loading';
 import { axios } from '../../services/Api';
+
 import * as UserTypes from '../../redux/ducks/types';
 
 import SignUpView from './SignUp';
-
 export type UserData = UserTypes.PersonalInformations;
+
+interface RootState {
+  loadingReducer: { loading: boolean };
+  userReducer: { data: User };
+}
 
 export default function Container() {
   const dispatch = useDispatch();
-  const selector = useSelector(state => state);
+  const loading = useSelector((state: RootState) => state.loadingReducer.loading);
+  const user = useSelector((state: RootState) => state.userReducer.data);
 
   const handleSignUp = (payload: UserData) => {
     return async () => {
       try {
-        const { data } = await axios.post('/user/create', payload);
+        dispatch(startLoading());
+        const { data } = await axios.post('user/create', payload, {
+          headers: { 'content-type': 'application/json' },
+        });
+        console.log(user);
         dispatch(createUser(data));
       } catch (error) {
+        console.log(error);
         throw new Error(error as string);
+      } finally {
+        dispatch(stopLoading());
       }
     };
   };
 
-  return <SignUpView handleSignUp={handleSignUp} />;
+  return <SignUpView handleSignUp={handleSignUp} loading={loading} />;
 }
